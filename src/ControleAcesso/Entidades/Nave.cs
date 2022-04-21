d3fe995aed7f2623eac187284464be8b14fcf179
Michele Carine <102440706+michelemeiira@users.noreply.github.com>
2022-04-21 16:08:50 -0300

    first commit

---
+++ src/ControleAcesso/Entidades/Nave.cs
@@ -0,0 +1,12 @@
+namespace ControleAcesso.Entidades
+{
+    public class Nave
+    {
+        public int IdNave { get; set; }
+        public string Nome { get; set; }
+        public string Modelo { get; set; }
+        public int Passageiros { get; set; }
+        public double Carga { get; set; }
+        public string Classe { get; set; }
+    }
+}
